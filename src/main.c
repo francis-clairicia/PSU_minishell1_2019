3a64b504fc529d1bf561ba5e1073a06ed7f1c1d6
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2020-01-26 15:22:07 +0100

    Removed set_shell_env() function in main.c

--- src/main.c
+++ src/main.c
@@ -16,25 +16,6 @@ static int print_help(void)
     return (0);
 }
 
-static int set_shell_env(char ***envp, char const *binary_name)
-{
-    char current_dir[4097];
-    int slash = my_find_char(binary_name, '/');
-    char *cmd[] = {"setenv", "SHELL", "", NULL};
-
-    if (envp == NULL || *envp == NULL)
-        return (0);
-    if (find_var_env(*envp, "SHELL") < 0)
-        return (1);
-    if (binary_name[0] != '/')
-        cmd[2] = join_path(getcwd(current_dir, 4097), &binary_name[slash + 1]);
-    else
-        cmd[2] = my_strdup(binary_name);
-    setenv_builtin_command(cmd, envp);
-    free(cmd[2]);
-    return (1);
-}
-
 static int command_prompt(char **line, int stop_shell)
 {
     char current_directory[4097];
@@ -64,7 +45,7 @@ int main(int ac, char **av)
     if (ac > 1 && my_strcmp(av[1], "-h") == 0)
         return (print_help());
     envp = copy_environment(DEFAULT_ENVIRONMENT);
-    if (!set_shell_env(&envp, av[0]))
+    if (envp == NULL)
         return (84);
     while (command_prompt(&cmd, stop_shell)) {
         stop_shell = minishell(cmd, &envp);

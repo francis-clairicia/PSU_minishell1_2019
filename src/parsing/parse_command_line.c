9a34a3a5f8dbba3c70324b375f6efbbaefc2b190
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2020-01-27 18:41:59 +0100

    Fixed little bug with parse_command_line() function

--- src/parsing/parse_command_line.c
+++ src/parsing/parse_command_line.c
@@ -17,7 +17,7 @@ static void unify_args(char **command, int arg_with_quote, char quote)
         return (remove_quotes(command, arg_with_quote, quote));
     while (command[other_quote] != NULL) {
         if (my_find_char(command[other_quote], quote) >= 0)
-            return create_arg(command, arg_with_quote, other_quote, quote);
+            return (create_arg(command, arg_with_quote, other_quote, quote));
         other_quote += 1;
     }
 }
@@ -25,19 +25,16 @@ static void unify_args(char **command, int arg_with_quote, char quote)
 char **parse_command_line(char const *command_line)
 {
     int i = 0;
-    char quote = 0;
     char separators[] = {' ', '\t', '\0'};
     char **command = my_str_to_word_array(command_line, separators);
 
     if (command == NULL)
         return (NULL);
     while (command[i] != NULL) {
-        quote = '\'';
-        if (my_find_char(command[i], quote) >= 0)
-            unify_args(command, i, quote);
-        quote = '"';
-        if (my_find_char(command[i], quote) >= 0)
-            unify_args(command, i, quote);
+        if (my_find_char(command[i], '\'') >= 0)
+            unify_args(command, i, '\'');
+        else if (my_find_char(command[i], '"') >= 0)
+            unify_args(command, i, '"');
         i += 1;
     }
     return (command);
